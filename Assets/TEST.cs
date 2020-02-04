a82507ffba04fbdf6e5f754c872ddeba8c090feb
andrea teruzzi <andre.teru93@gmail.com>
2020-02-04 17:28:02 +0100

    more some stuff

---
+++ Assets/TEST.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TEST : MonoBehaviour
+{
+    public UnityEvent ue;
+    public int n;
+
+    private void Update()
+    {
+        for(int i = 0; i < n; i++)
+        {
+            ue.Invoke();
+        }
+    }
+}
